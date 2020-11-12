373691fb0c3901bb4a6179f79160764849cf1ef0
Piotr Misiejuk <piotr.misiejuk@pragmatists.pl>
2020-11-12 14:16:17 +0100

    Generator moved to vivid-react (#1)
    * CY-841 - generator moved from vivid-react-wrapper (CY-731-wrappers-generator branch)
    
    * CY-841 - rmdir => rm -f
    
    * Revert "CY-841 - rmdir => rm -f"
    
    This reverts commit 591accf3
    
    * CY-841 add .gitignore records for generator temp and generated files
    
    * CY-831, include component import into wrapper
    
    * CY-831, include component import into wrapper [both use cases]
    
    * CY-831, clean up temp at the end of processing
    
    * PR fixes #1
    
    * PR fixes #2
    
    Co-authored-by: Alexander Mazuruk <alexander.mazuruk@gmail.com>
    Co-authored-by: Adam Trojanowski <adam.trojanowski@pragmatists.pl>

---
+++ bin/cli.js
@@ -0,0 +1,28 @@
+const { OutputLanguage, CLIArgument, WCAConfigAll } = require('../src/consts')
+const { generateWrappers } = require('../src/generator')
+const {
+  isFileExists,
+  getParsedJson,
+  getVividPackageNames,
+  getInputArgument,
+  getVividLatestRelease,
+  getCustomElementTagsDefinitionsList
+} = require('../src/utils')
+
+const OutputDirectory = './dist'
+
+getInputArgument(CLIArgument.All)
+  ? getVividLatestRelease()
+    .then(getCustomElementTagsDefinitionsList(WCAConfigAll))
+    .then(generateWrappers(
+      getInputArgument(CLIArgument.Output, OutputDirectory),
+      getInputArgument(CLIArgument.Language, OutputLanguage.JavaScript)
+    ))
+  : isFileExists('package.json')
+    .then(getParsedJson)
+    .then(getVividPackageNames)
+    .then(getCustomElementTagsDefinitionsList())
+    .then(generateWrappers(
+      getInputArgument(CLIArgument.Output, OutputDirectory),
+      getInputArgument(CLIArgument.Language, OutputLanguage.JavaScript)
+    ))
---
+++ src/consts.js
@@ -0,0 +1,45 @@
+const tempFolder = 'temp'
+
+const WCAConfig = {
+  tempFolder,
+  tempFileName: 'analyzerOutput.json',
+  nodeArgumentsFactory: (packageNames, analyzerOutputFile) => [
+    './node_modules/web-component-analyzer/cli.js',
+    'analyze',
+    `node_modules/{${packageNames.join(',')}}/{src/,}*.?s`,
+    '--discoverNodeModules',
+    '--format', 'json',
+    '--outFile', analyzerOutputFile
+  ]
+}
+
+const WCAConfigAll = {
+  tempFolder,
+  tempFileName: 'analyzerOutput.json',
+  nodeArgumentsFactory: (targetFolder, analyzerOutputFile) => [
+    './node_modules/web-component-analyzer/cli.js',
+    'analyze',
+    `${targetFolder}/{src/,}*.?s`,
+    '--format', 'json',
+    '--outFile', analyzerOutputFile
+  ]
+}
+
+const CLIArgument = {
+  Output: 'output', // output folder
+  Language: 'language', // language js,ts
+  All: 'all' // ignore local package.json and produce wrappers for *all* Vivid components
+}
+
+const OutputLanguage = {
+  JavaScript: 'js',
+  TypeScript: 'ts'
+}
+
+module.exports = {
+  OutputLanguage,
+  WCAConfig,
+  WCAConfigAll,
+  CLIArgument,
+  tempFolder
+}

067c4e4cec3e23739683bf0b2271def9d62ffb46
Alexander Mazuruk <alexander.mazuruk@gmail.com>
2021-04-19 21:47:07 +0200

    Update consts.js

--- scripts/generateWrappers/consts.js
+++ scripts/generateWrappers/consts.js
@@ -84,7 +84,7 @@ const ComponentsReadOnlyPropertiesMap = {
   VwcTabBar: ['styles', 'active'],
   VwcTextarea: ['styles', 'validity'],
   VwcTextfield: ['styles', 'validity', 'willValidate', 'selectionStart', 'selectionEnd', 'ripple'],
-  VwcMenu: ['styles', 'slotElement', 'items'],
+  VwcMenu: ['styles', 'slotElement', 'items', 'index', 'selected'],
   VwcRadio: ['styles', 'isRippleActive'],
   VwcRadioListItem: ['styles', 'ripple'],
   VwcSelect: ['styles', 'items'],

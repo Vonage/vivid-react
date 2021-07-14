c7e727e30224bfd785ab99e525a457c44762aca2
Alexander Mazuruk <alexander.mazuruk@gmail.com>
2021-07-14 11:49:34 +0200

    escape html in jsdoc

--- scripts/generateWrappers/utils.js
+++ scripts/generateWrappers/utils.js
@@ -9,7 +9,7 @@ const { Octokit } = require('@octokit/core')
 const extract = require('extract-zip')
 const { copySync } = require('fs-extra')
 
-const renderPropertyJsDoc = tag => property => `* @param ${property.type ? `{${property.type}}` : ''} ${property.name} ${property.description ? `- ${property.description}` : ''} ${property.attribute ? `attribute: <${getComponentName(tag)} ${property.attribute} />` : ''}`
+const renderPropertyJsDoc = tag => property => `* @param ${property.type ? `{${property.type}}` : ''} ${property.name} ${property.description ? `- ${property.description}` : ''} ${property.attribute ? `attribute: &lt;${getComponentName(tag)} ${property.attribute} />` : ''}`
 const renderTagPropertiesJsDoc = tag => getProperties(tag).map(renderPropertyJsDoc(tag)).join('\n')
 const renderJsDoc = tag => `/** ${tag.description || ''} \n${renderTagPropertiesJsDoc(tag)}\n*/`
 const stripQuotes = input => input.replace(/'/g, '')

75b1406c91c0ecaf864135daa7e1d1b96068d9cf
Alexander Mazuruk <alexander.mazuruk@gmail.com>
2022-06-23 14:52:47 +0200

    Update VwcList.stories.jsx

--- stories/VwcList/VwcList.stories.jsx
+++ stories/VwcList/VwcList.stories.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
+import VwcCheckListItem from '../../dist/VwcCheckListItem'
 import VwcList from '../../dist/VwcList'
 
-export const Default = () => <VwcList />
+export const Default = () => (
+  <VwcList>
+    <VwcCheckListItem />
+    <VwcCheckListItem />
+    <VwcCheckListItem />
+  </VwcList>
+)
 export default {
   title: 'VwcList',
   argTypes: {
-      //
-      // Example values
-      // numberValue: { control: 'number', defaultValue: 123 },
-      // booleanValue: { control: 'boolean', defaultValue: true },
-      // objectValue: { control: 'object', defaultValue: {} },
-      // stringValue: { control: 'string', defaultValue: 'string' },
-      // colorValue: { control: 'color' },
-      // dateValue: { control: 'date' }
-      //
-    }
+    //
+    // Example values
+    // numberValue: { control: 'number', defaultValue: 123 },
+    // booleanValue: { control: 'boolean', defaultValue: true },
+    // objectValue: { control: 'object', defaultValue: {} },
+    // stringValue: { control: 'string', defaultValue: 'string' },
+    // colorValue: { control: 'color' },
+    // dateValue: { control: 'date' }
+    //
+  }
 }

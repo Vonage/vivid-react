62802f1a885c4de01a54f51d4a4ff7487988bfa0
Abhijeet Jadhav <110904215+abhijeetj10@users.noreply.github.com>
2024-06-20 10:21:23 +0200

    feat(badge-patch): fix for badge text center (#67)
    * feat(badge-patch): fix for badge text center
    
    * feat(fixed): empty line
    
    * feat(badge): fix comments

---
+++ src/patches/vwcBadge.js
@@ -0,0 +1,21 @@
+/**
+* Patches provided vivid-3 VwcBadge instance to show text in the center
+*
+* @param {HTMLElement} badgeElement - target element to apply behavior to
+* @example <VwcBadge ref={vwcBadgeShowTextCenterDecorator()} />
+*/
+export const vwcBadgeShowTextCenterDecorator = () => badgeElement =>
+    setTimeout(() => {
+        if (!badgeElement) {
+            return
+        }
+        
+        if (badgeElement.shadowRoot) {
+            const span = badgeElement.shadowRoot.querySelector('span.base');
+            if (span) {
+                span.style.display = 'flex';
+                span.style.justifyContent = 'center';
+            }
+        }
+    }, 0)
+
